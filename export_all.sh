49897d403b39c752cfa85cc2aadb8df5068015cf
Relintai <relintai@protonmail.com>
2023-06-01 00:21:59 +0200

    Initial commit.

---
+++ export_all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./pandemonium_engine/misc/scripts_app/export_all.sh gdnative_sample gdns $1
+
---
+++ make_release.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./pandemonium_engine/misc/scripts_app/make_release.sh gdnative_sample gdns $1
+
