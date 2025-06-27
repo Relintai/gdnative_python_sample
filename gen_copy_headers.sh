f86f56f35a66193a6e584ed7de1e037600a85691
Relintai <relintai@relintai.net>
2025-06-27 08:17:27 +0200

    Check for the existence of folders in gen_copy_headers.sh.

--- gen_copy_headers.sh
+++ gen_copy_headers.sh
@@ -1,5 +1,16 @@
 
+
+if [ ! -d "./pandemonium_engine/" ]; then
+  echo "Folder ./pandemonium_engine/ does exist. Aborting."
+  exit 1
+fi
+
 ./pandemonium_engine/bin/pandemonium.x11.opt.tools.64 --gdnative-generate-json-api ./pandemonium_engine/modules/gdnative/api.json
+
+if [ ! -d "gdnative_python/pandemonium_headers/" ]; then
+  echo "Folder gdnative_python/pandemonium_headers/ does exist. Aborting."
+  exit 1
+fi
 
 cd gdnative_python/pandemonium_headers/
 rm -Rf *
